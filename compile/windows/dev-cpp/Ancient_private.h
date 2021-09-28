c42091a7a53ecea73bce9ab7a7958919dc0f1f2a
Ramon Bernardo <ramonbernardo.contato@gmail.com>
2021-09-28 08:41:26 -0300

    Compile on windows with dev-cpp
    default compile flags:
    __USE_MYSQL__
    __USE_SQLITE__
    __ENABLE_SERVER_DIAGNOSTIC__
    __WAR_SYSTEM__
    with O2 compress.

---
+++ compile/windows/dev-cpp/Ancient_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ANCIENT_PRIVATE_H
+#define ANCIENT_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Open Tibia"
+#define FILE_VERSION	"1.0"
+#define FILE_DESCRIPTION	"Ancient"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"Ancient.exe"
+#define PRODUCT_NAME	"Ancient"
+#define PRODUCT_VERSION	"1.0"
+
+#endif /*ANCIENT_PRIVATE_H*/
